b189b95badcaf85aa9c3553bd05bd580d0d02b8e
barash <assafba@wix.com>
2020-10-13 11:58:41 +0300

    refactor(main): seperate code

--- index.js
+++ index.js
@@ -2,111 +2,40 @@
 
 const { exec } = require('child_process');
 const prompts = require('prompts');
+const { commitMessageParams, executionFlow } = require('./prompts');
 
-const executeCommand = (cmd) =>
-  new Promise((resolve, reject) =>
-    exec(cmd, (err, stdout, stderr) =>
-      err ? reject({ err, stderr }) : resolve(stdout)
-    )
-  );
-
-const createChoice = ({ value, title = value, description }) => ({
-  title,
-  value,
-  description,
-});
-
-(async () => {
-  const { scope, description, type, ticketNumber, crs } = await prompts([
-    {
-      type: 'autocomplete',
-      name: 'type',
-      message: 'Select commit type',
-      choices: [
-        'feat',
-        'fix',
-        'chore',
-        'refactor',
-        'test',
-        'revert',
-        'build',
-        'ci',
-        'docs',
-        'perf',
-        'style',
-      ].map((commitType) => createChoice({ value: commitType })),
-    },
-    {
-      type: 'text',
-      name: 'scope',
-      message: 'Enter scope',
-      format: (scope) => (scope ? `(${scope})` : ''),
-    },
-    {
-      type: 'text',
-      name: 'description',
-      message: 'Enter description',
-      validate: Boolean,
-    },
-    {
-      type: 'text',
-      name: 'ticketNumber',
-      message: 'Enter ticket number',
-    },
-    {
-      type: 'text',
-      name: 'crs',
-      message: 'Enter CRs (separated by ,)',
-      initial: '',
-      format: (crs) =>
-        crs
-          ? `#cr${crs
-              .split(',')
-              .map((cr) => `@${cr}`)
-              .join('')}`
-          : '',
-    },
-  ]);
-
-  const commitHead = `-m "${type}${scope}: ${description}"`;
-  const commitBody = ticketNumber || crs ? `-m "${ticketNumber} ${crs}"` : '';
-  const commitCommand = `git commit ${commitHead} ${commitBody}"`;
+async function main() {
+  const commitCommand = buildCommitMessage(await prompts(commitMessageParams));
 
   console.log(commitCommand);
 
-  const { commit, pull, push, addDir } = await prompts([
-    {
-      type: 'confirm',
-      name: 'addDir',
-      message: 'Add current directory?',
-      initial: true,
-    },
-    {
-      type: 'confirm',
-      name: 'commit',
-      message: 'Execute the above commit commit?',
-      initial: true,
-    },
-    {
-      type: (prev) => (prev ? 'confirm' : null),
-      name: 'pull',
-      message: 'Execute pull and rebase?',
-      initial: true,
-    },
-    {
-      type: (prev) => (prev ? 'confirm' : null),
-      name: 'push',
-      message: 'Execute push?',
-      initial: true,
-    },
-  ]);
+  executeCommit(commitCommand, await prompts(executionFlow));
+}
+
+function buildCommitMessage({ scope, description, type, ticketNumber, crs }) {
+  const commitHead = `-m "${type}${scope}: ${description}"`;
+  const commitBody = ticketNumber || crs ? `-m "${ticketNumber} ${crs}"` : '';
+  return `git commit ${commitHead} ${commitBody}`;
+}
+
+async function executeCommit(commitCommand, { commit, pull, push, addDir }) {
+  const executeCommand = (cmd) =>
+    new Promise((resolve, reject) =>
+      exec(cmd, (err, stdout, stderr) =>
+        err ? reject({ err, stderr }) : resolve(stdout)
+      )
+    );
 
   addDir && (await executeCommand('git add .'));
-  commit &&
+  return (
+    commit &&
     executeCommand(commitCommand)
       .then(() => pull && executeCommand('git pull --rebase --autostash'))
       .then(() => push && executeCommand('git push'))
       .then(() => console.log('SUCCESS'))
       .then(process.exit)
-      .catch(console.error);
-})();
+      .catch(console.error)
+  );
+}
+
+main();
---
+++ prompts.js
@@ -0,0 +1,85 @@
+const createChoice = ({ value, title = value, description }) => ({
+  title,
+  value,
+  description,
+});
+
+const commitMessageParams = [
+  {
+    type: 'autocomplete',
+    name: 'type',
+    message: 'Select commit type',
+    choices: [
+      'feat',
+      'fix',
+      'chore',
+      'refactor',
+      'test',
+      'revert',
+      'build',
+      'ci',
+      'docs',
+      'perf',
+      'style',
+    ].map((commitType) => createChoice({ value: commitType })),
+  },
+  {
+    type: 'text',
+    name: 'scope',
+    message: 'Enter scope',
+    format: (scope) => (scope ? `(${scope})` : ''),
+  },
+  {
+    type: 'text',
+    name: 'description',
+    message: 'Enter description',
+    validate: Boolean,
+  },
+  {
+    type: 'text',
+    name: 'ticketNumber',
+    message: 'Enter ticket number',
+  },
+  {
+    type: 'text',
+    name: 'crs',
+    message: 'Enter CRs (separated by ,)',
+    initial: '',
+    format: (crs) =>
+      crs
+        ? `#cr${crs
+            .split(',')
+            .map((cr) => `@${cr}`)
+            .join('')}`
+        : '',
+  },
+];
+
+const executionFlow = [
+  {
+    type: 'confirm',
+    name: 'addDir',
+    message: 'Add current directory?',
+    initial: true,
+  },
+  {
+    type: 'confirm',
+    name: 'commit',
+    message: 'Execute the above commit commit?',
+    initial: true,
+  },
+  {
+    type: (prev) => (prev ? 'confirm' : null),
+    name: 'pull',
+    message: 'Execute pull and rebase?',
+    initial: true,
+  },
+  {
+    type: (prev) => (prev ? 'confirm' : null),
+    name: 'push',
+    message: 'Execute push?',
+    initial: true,
+  },
+];
+
+module.exports = { executionFlow, commitMessageParams };
