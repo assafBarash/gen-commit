950857e78f5e388df521c3103305ccddd40d09a5
barash <assafba@wix.com>
2020-11-17 15:42:18 +0200

    fix(git-commit): fix message sections read flow

--- src/message-section-handlers.js
+++ src/message-section-handlers.js
@@ -5,12 +5,12 @@ const { lookup, asyncMap } = require('./utils');
 const CONFIG_FILE = 'commit-generator.config';
 
 const getConfigDir = () => lookup(`${CONFIG_FILE}.js`);
-const readConfig = async (overrideDir, ...args) => {
+const readConfig = (overrideDir, ...args) => {
   try {
     const config = require(`${overrideDir || getConfigDir()}`);
 
     if (typeof config === 'object') return Promise.resolve(config);
-    if (typeof config === 'function') return config(args);
+    if (typeof config === 'function') return config(...args);
   } catch (e) {
     overrideDir &&
       console.error(
@@ -42,14 +42,19 @@ const getMessageSections = async (flags) => {
   const parsedDefConfig = defConfig(flags);
   const res = await readConfig(overrideConfig, parsedDefConfig, flags);
 
-  if (Array.isArray(res)) return [parsedDefConfig, ...res];
+  if (Array.isArray(res)) return res;
   if (typeof res === 'object') return res(parsedDefConfig, flags);
 
   return console.log('failed to read config') || [];
 };
 
 const buildMessageSections = async (flags) =>
-  (await asyncMap(await getMessageSections(flags), buildMessageSection))
+  (
+    await asyncMap(
+      (await getMessageSections(flags)).filter(Boolean),
+      buildMessageSection
+    )
+  )
     .filter(Boolean)
     .map((str) => `"${str}"`)
     .join(' -m ');
